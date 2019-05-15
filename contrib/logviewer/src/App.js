b50989537795bbea812f20667440f699cedbffbb
Spike^ekipS <spikeekips@gmail.com>
2019-05-15 12:27:38 +0900

    record detail

--- contrib/logviewer/src/App.js
+++ contrib/logviewer/src/App.js
@@ -169,16 +169,20 @@ class CenteredGrid extends React.Component {
   openDetail(ref, open) {
     const tr = ReactDOM.findDOMNode(ref.current)
     if (open === undefined) {
-      tr.classList.toggle('row-detail-open')
+      tr.nextSibling.classList.toggle('row-detail-open')
       return
     }
 
     if (open === true) {
-      tr.classList.add('row-detail-open')
+      tr.nextSibling.classList.add('row-detail-open')
     } else {
-      tr.classList.remove('row-detail-open')
+      tr.nextSibling.classList.remove('row-detail-open')
     }
     return
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return true
   }
 
   renderRecord(first, record, nodes) {
@@ -188,20 +192,19 @@ class CenteredGrid extends React.Component {
     if (i < 0) {
       return null
     }
-    
+
     var rowRef = React.createRef()
     var rowDetailRef = React.createRef()
-    
-    return (
-    <React.Fragment key={record.id + 'f'}>
+
+    return <React.Fragment key={record.id + 'f'}>
       <TableRow key={record.id} ref={rowRef}>
         <TableCell key={record.id + '-t'}>
           <IconButton className={classes.button} aria-label='Bookmark' onClick={e => {
             const tr = ReactDOM.findDOMNode(rowRef.current)
             tr.classList.toggle('selected')
             tr.nextSibling.classList.toggle('selected')
-    
-            this.openDetail(rowDetailRef, true)
+
+            this.openDetail(rowRef, true)
           }}>
             <BookmarksIcon />
           </IconButton>
@@ -214,7 +217,7 @@ class CenteredGrid extends React.Component {
           <TableCell
             className={classes.listTableTd}
             key={record.id + node + '-m'}
-            onClick={e => this.openDetail(rowDetailRef)}
+            onClick={e => this.openDetail(rowRef)}
           >
           {i === index ? (
               <Typography key={record.id + node + 'ty'}>{record.message}</Typography>
@@ -225,20 +228,8 @@ class CenteredGrid extends React.Component {
           </TableCell>
         ))}
       </TableRow>
-      <TableRow className={'row-detail'} key={record.id + 'detail'} ref={rowDetailRef}>
-        <TableCell colSpan={nodes.length + 1}>
-          <Grid container className={classes.root} spacing={16}>
-            <Grid item xs={4}>
-              <Highlight className='json'>{JSON.stringify(record.basic(), null, 2)}</Highlight>
-            </Grid>
-            <Grid item xs={8}>
-              <Highlight className='json'>{JSON.stringify(record.extra, null, 2)}</Highlight>
-            </Grid>
-          </Grid>
-        </TableCell>
-      </TableRow>
+      <RecordDetail classes={classes} nodes={nodes} record={record} ref={rowDetailRef} />
     </React.Fragment>
-    )
   }
 
   renderRecords(records, nodes) {
@@ -357,6 +348,25 @@ class IntegrationNotistack extends React.Component {
         <MyApp {...this.props} />
       </SnackbarProvider>
     )
+  }
+}
+
+class RecordDetail extends React.Component {
+  render() {
+    const { record, nodes, classes } = this.props;
+
+    return <TableRow className={'row-detail'} key={record.id + 'detail'}>
+      <TableCell colSpan={nodes.length + 1}>
+        <Grid container className={classes.root} spacing={16}>
+          <Grid item xs={4}>
+            <Highlight className='json'>{JSON.stringify(record.basic(), null, 2)}</Highlight>
+          </Grid>
+          <Grid item xs={8}>
+            <Highlight className='json'>{JSON.stringify(record.extra, null, 2)}</Highlight>
+          </Grid>
+        </Grid>
+      </TableCell>
+    </TableRow>
   }
 }
 
